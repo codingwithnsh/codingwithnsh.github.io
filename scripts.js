eedc24a2a2390d9efd630865bb1d5883aa6b6e88
Naman S <138281862+codingwithnsh@users.noreply.github.com>
2024-08-07 23:34:05 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
+$(document).ready(function() {
+    $('.fa-bars').click(function() {
+        $('.nav-screen').fadeIn();
+    });
+
+    $('.fa-times').click(function() {
+        $('.nav-screen').fadeOut();
+    });
+});
 
